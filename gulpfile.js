410e4cbb50b6d429d7a10c7c71349936b44ca416
Darya <72408903+prokhorovadaria@users.noreply.github.com>
2023-01-29 23:23:09 +0300

    Top-menu

--- gulpfile.js
+++ gulpfile.js
@@ -35,8 +35,9 @@ gulp.task('code', function() {
 });
  
 gulp.task('watch', function() {
-	gulp.watch('app/sass/**/*.sass', gulp.parallel('sass')); // Наблюдение за sass файлами
+	gulp.watch('app/sass/main.sass', gulp.parallel('sass')); // Наблюдение за sass файлами
 	gulp.watch('app/*.html', gulp.parallel('code')); // Наблюдение за HTML файлами в корне проекта
 	gulp.watch(['app/js/common.js', 'app/libs/**/*.js'], gulp.parallel('scripts')); // Наблюдение за главным JS файлом и за библиотеками
 });
 gulp.task('default', gulp.parallel('sass', 'browser-sync', 'watch'));
+
